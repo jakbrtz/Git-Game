b9b4e0070da0b4a3f15cd5237acb3bf712406fe7
Jake <jake@bortz.com.au>
2023-05-03 21:56:30 +1000

    List actions

--- GameRunner.py
+++ GameRunner.py
@@ -3,18 +3,24 @@ import os
 import sys
 
 level = "Levels/00 Sample Level"
-path = level + "/SaveFile.json"
+saveFilePath = level + "/SaveFile.json"
 
 sys.dont_write_bytecode = True
 sys.path.append(level)
 import Script
 
-if os.path.exists(path):
-    with open(path) as f:
-        data = json.load(f)
+if os.path.exists(saveFilePath):
+    with open(saveFilePath) as myfile:
+        data = json.load(myfile)
 else:
     data = Script.InitialFile()
-    with open(path, "w") as f:
-        json.dump(data, f, indent = 4)
+    with open(saveFilePath, "w") as myfile:
+        json.dump(data, myfile, indent = 4)
 
 print(Script.GetDescription(data))
+# todo: write to file?
+
+for action in Script.GetActions(data):
+    print(action[0])
+    #for instruction in action[1]:
+    #    myfile.write(f"data[\"{instruction[0]}\"] = \"{instruction[1]}\"\n")

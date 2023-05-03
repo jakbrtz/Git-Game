4eb644c173a01b2caeb9bf111460eca8a1f630e4
Jake <jake@bortz.com.au>
2023-05-03 21:56:30 +1000

    Populate actions folder

--- GameRunner.py
+++ GameRunner.py
@@ -3,18 +3,39 @@ import os
 import sys
 
 level = "Levels/00 Sample Level"
-path = level + "/SaveFile.json"
+saveFilePath = level + "/SaveFile.json"
+actionsDirectory = level + "/actions"
 
 sys.dont_write_bytecode = True
 sys.path.append(level)
 import Script
 
-if os.path.exists(path):
-    with open(path) as f:
-        data = json.load(f)
+if os.path.exists(saveFilePath):
+    with open(saveFilePath) as myfile:
+        data = json.load(myfile)
 else:
     data = Script.InitialFile()
-    with open(path, "w") as f:
-        json.dump(data, f, indent = 4)
+    with open(saveFilePath, "w") as myfile:
+        json.dump(data, myfile, indent = 4)
 
 print(Script.GetDescription(data))
+# todo: write to file?
+
+if os.path.isdir(actionsDirectory):
+    for file in os.listdir(actionsDirectory):
+        os.remove(actionsDirectory + "/" + file)
+else:
+    os.mkdir(actionsDirectory)
+
+for action in Script.GetActions(data):
+    with open(f"{actionsDirectory}/{action[0]}.py", "a+") as myfile:
+        myfile.write("import json\n")
+        myfile.write("\n")
+        myfile.write("with open(\"../SaveFile.json\") as myfile:\n")
+        myfile.write("    data = json.load(myfile)\n")
+        myfile.write("\n")
+        for instruction in action[1]:
+            myfile.write(f"data[\"{instruction[0]}\"] = \"{instruction[1]}\"\n")
+        myfile.write("\n")
+        myfile.write("with open(\"../SaveFile.json\", \"w\") as myfile:\n")
+        myfile.write("    json.dump(data, myfile, indent = 4)\n")

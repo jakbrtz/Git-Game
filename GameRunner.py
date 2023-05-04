3b1748a71db0448c4c2f6bf7158114b36d9f5d28
Jake <jake@bortz.com.au>
2023-05-04 18:53:32 +1000

    Now does git commits

--- GameRunner.py
+++ GameRunner.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 import sys
 
 level = "Levels/00 Sample Level"
@@ -17,6 +18,11 @@ else:
     with open(saveFilePath, "w") as myfile:
         json.dump(data, myfile, indent = 4)
 
+if not os.path.isdir(level + "/.git"):
+    subprocess.call("git init", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+    subprocess.call("git add .", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+    subprocess.call("git commit -m start", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+
 print(Script.GetDescription(data))
 
 actions = Script.GetActions(data)
@@ -24,9 +30,12 @@ actions = Script.GetActions(data)
 for i, action in enumerate(actions):
     print(f"{i}. {action[0]}")
 
-chosen = int(input())
-for instruction in actions[chosen][1]:
+chosen = actions[int(input())]
+for instruction in chosen[1]:
     data[instruction[0]] = instruction[1]
 
 with open(saveFilePath, "w") as myfile:
     json.dump(data, myfile, indent = 4)
+
+subprocess.call("git add .", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+subprocess.call("git commit -m " + chosen[0], shell=True, cwd=level, stdout=subprocess.DEVNULL)

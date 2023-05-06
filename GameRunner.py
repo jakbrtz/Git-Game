6664b33fe20674c3a674e61b58d6457e6ed0de32
Jake <jake@bortz.com.au>
2023-05-06 13:16:17 +1000

    Put extra lines in save file to avoid merge conflicts

--- GameRunner.py
+++ GameRunner.py
@@ -16,7 +16,7 @@ if os.path.exists(saveFilePath):
 else:
     data = Script.InitialFile()
     with open(saveFilePath, "w") as myfile:
-        json.dump(data, myfile, indent = 4)
+        json.dump(data, myfile, indent = 4, separators = (",\n", ":"))
 
 if not os.path.isdir(level + "/.git"):
     subprocess.call("git init", shell=True, cwd=level, stdout=subprocess.DEVNULL)
@@ -37,7 +37,7 @@ chosen = actions[int(input())]
 data.update(chosen[1])
 
 with open(saveFilePath, "w") as myfile:
-    json.dump(data, myfile, indent = 4)
+    json.dump(data, myfile, indent = 4, separators = (",\n", ":"))
 
 subprocess.call("git add .", shell=True, cwd=level, stdout=subprocess.DEVNULL)
 subprocess.call("git commit -m \"" + chosen[0] + "\"", shell=True, cwd=level, stdout=subprocess.DEVNULL)

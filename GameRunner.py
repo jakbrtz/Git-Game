a626daaa094bef2fa1b2e7d33c717d43d078d63a
Jake <jake@bortz.com.au>
2023-05-08 18:28:49 +1000

    Run game without leaving directory

--- GameRunner.py
+++ GameRunner.py
@@ -3,12 +3,20 @@ import os
 import subprocess
 import sys
 
-level = "Levels/03 Zipline"
-saveFilePath = level + "/SaveFile.json"
-
+sys.path.append("")
 sys.dont_write_bytecode = True
-sys.path.append(level)
-import Script
+try:
+    import Script
+except:
+    print("Script not found. ")
+    print("Make sure you're in the script's directory when you open this file.")
+    print("To do this, navigate to the file with the Script module and then:")
+    print(" - run the `play.bat` file if one exists")
+    print(" - run this file using with: python \"" + os.path.abspath(sys.argv[0]) + "\"")
+    print(" - copy this file into the script's folder")
+    exit()
+
+saveFilePath = "SaveFile.json"
 
 if os.path.exists(saveFilePath):
     with open(saveFilePath) as myfile:
@@ -18,10 +26,10 @@ else:
     with open(saveFilePath, "w") as myfile:
         json.dump(data, myfile, indent = 4, separators = (",\n", ": "))
 
-if not os.path.isdir(level + "/.git"):
-    subprocess.call("git init", shell=True, cwd=level, stdout=subprocess.DEVNULL)
-    subprocess.call("git add .", shell=True, cwd=level, stdout=subprocess.DEVNULL)
-    subprocess.call("git commit -m start", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+if not os.path.isdir(".git"):
+    subprocess.call("git init", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call("git add .", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call("git commit -m start", shell=True, stdout=subprocess.DEVNULL)
 
 while True:
 
@@ -35,7 +43,7 @@ while True:
     for i, action in enumerate(actions):
         print(f"{i}. {action[0]}")
 
-    gitHead1 = subprocess.check_output("git rev-parse HEAD", shell=True, cwd=level)
+    gitHead1 = subprocess.check_output("git rev-parse HEAD", shell=True)
 
     chosen = actions[int(input())]
     if (callable(chosen[1])):
@@ -44,7 +52,7 @@ while True:
         data.update(chosen[1])
     print()
 
-    gitHead2 = subprocess.check_output("git rev-parse HEAD", shell=True, cwd=level)
+    gitHead2 = subprocess.check_output("git rev-parse HEAD", shell=True)
 
     if gitHead1 != gitHead2:
         print("You cannot perform an action anymore because the git head changed. Please try again. \n")
@@ -55,5 +63,5 @@ while True:
     with open(saveFilePath, "w") as myfile:
         json.dump(data, myfile, indent = 4, separators = (",\n", ": "))
 
-    subprocess.call("git add .", shell=True, cwd=level, stdout=subprocess.DEVNULL)
-    subprocess.call("git commit -m \"" + chosen[0] + "\"", shell=True, cwd=level, stdout=subprocess.DEVNULL)
+    subprocess.call("git add .", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call("git commit -m \"" + chosen[0] + "\"", shell=True, stdout=subprocess.DEVNULL)
---
+++ Levels/06 Ladders/play.bat
@@ -0,0 +1,1 @@
+python ./../../GameRunner.py

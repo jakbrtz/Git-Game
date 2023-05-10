9780a7508b7e435b88232e78a8e59b1a6a4706d9
Jake <jake@bortz.com.au>
2023-05-10 21:55:09 +1000

    Comments

--- GameRunner.py
+++ GameRunner.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 
+# Search for Script.py
 sys.path.append("")
 sys.dont_write_bytecode = True
 try:
@@ -16,8 +17,8 @@ except:
     print(" - copy this file into the script's folder")
     exit()
 
+# Load the save file, or create a new one if it doesn't exist
 saveFilePath = "SaveFile.json"
-
 if os.path.exists(saveFilePath):
     with open(saveFilePath) as myfile:
         data = json.load(myfile)
@@ -26,22 +27,29 @@ else:
     with open(saveFilePath, "w") as myfile:
         json.dump(data, myfile, indent = 4, separators = (",\n", ": "))
 
+# Create a git repository if it doesn't exist
 if not os.path.isdir(".git"):
     subprocess.call("git init", shell=True, stdout=subprocess.DEVNULL)
     subprocess.call("git add .", shell=True, stdout=subprocess.DEVNULL)
     subprocess.call("git commit -m start", shell=True, stdout=subprocess.DEVNULL)
 
+# The game loop:
 while True:
 
+    # Tell the user what's going on and get the list of possible actions
     print()
     actions = Script.DescribeAndGetActions(data)
     print()
+
+    # If no actions are available then exit
     if len(actions) == 0:
         print("No actions available")
         exit()
 
+    # Before waiting for user input, record the current save file
     gitHead1 = subprocess.check_output("git rev-parse HEAD", shell=True)
 
+    # Ask the user to pick an action
     print("Available actions:")
     for i, action in enumerate(actions):
         print(f"{i}. {action[0]}")
@@ -53,6 +61,7 @@ while True:
         print("Exiting...")
         exit()
 
+    # Compare the active save file to the actual save file
     gitHead2 = subprocess.check_output("git rev-parse HEAD", shell=True)
     if gitHead1 != gitHead2:
         print("You cannot perform an action anymore because the git head changed. Please try again. \n")
@@ -60,13 +69,16 @@ while True:
             data = json.load(myfile)
         continue
 
+    # Update the save file
     if (callable(chosen[1])):
         data.update(chosen[1]())
     else:
         data.update(chosen[1])
 
+    # Save the save file
     with open(saveFilePath, "w") as myfile:
         json.dump(data, myfile, indent = 4, separators = (",\n", ": "))
 
+    # git commit the changes
     subprocess.call("git add .", shell=True, stdout=subprocess.DEVNULL)
     subprocess.call("git commit -m \"" + chosen[0] + "\"", shell=True, stdout=subprocess.DEVNULL)
